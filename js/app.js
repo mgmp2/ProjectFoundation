a417f6e892af6f38a96e5236005b5397f9eb60a1
Misheca <Misheca>
2017-05-23 13:55:14 -0500

    add description product details

--- js/app.js
+++ js/app.js
@@ -8,3 +8,38 @@ $(document).on('ready', function() {
       slidesToScroll: 3
     });
   });
+
+$("#imgmain").elevateZoom();
+
+window.addEventListener("load", function(event) {
+  var productDetails = document.getElementById('productDetails');
+  var opendetails = document.getElementById('opendetails');
+  var catalogProductSeasons = document.getElementById('catalogProductSeasons');
+  var catalogProductSeasons2 = document.getElementById('catalogProductSeasons2');
+
+  productDetails.classList.add("classnone");
+
+  opendetails.addEventListener("click", function () {
+      productDetails.classList.remove("classnone");
+      catalogProductSeasons.classList.add("classnone");
+      catalogProductSeasons2.classList.add("classnone");
+  });
+
+  var imgsecondary1 = document.getElementById("imgsecondary1").getAttribute("src");
+  var imgsecondary2 = document.getElementById("imgsecondary2").getAttribute("src");
+  var imgsecondary3 = document.getElementById("imgsecondary3").getAttribute("src");
+
+  document.getElementById("aimgsecondary1").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("imgmain").setAttribute("src",imgsecondary1);
+  });
+  document.getElementById("aimgsecondary2").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("imgmain").setAttribute("src",imgsecondary2);
+  });
+  document.getElementById("aimgsecondary3").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("imgmain").setAttribute("src",imgsecondary3);
+  });
+
+});
